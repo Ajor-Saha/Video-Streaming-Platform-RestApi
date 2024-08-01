ec95201c01bbab48cf6b6c438c74068aacb5c653
ajor-saha <sokarama79@gmail.com>
2024-08-01 23:49:42 +0600

    add vercel.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
 
 dotenv.config({
     path: './.env'
